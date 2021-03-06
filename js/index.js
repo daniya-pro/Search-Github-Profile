9c8380cd0c2500687cf4b6bcc4c9b0e0671b8481
daniya-pro <syedadaniyahaider@gmail.com>
2021-03-06 23:53:41 +0500

    fixed some errors

---
+++ js/index.js
@@ -0,0 +1,21 @@
+function executeData(){
+    var userName =document.getElementById('userName').value
+    if(userName){
+    
+    fetch(`https://api.github.com/users/${userName}`).then(
+(res)=>res.json()
+)
+.then(data=>{
+//  avatar_url" 
+     document.getElementById('YourUserName').innerHTML=data.login
+     document.getElementById('joinedAt').innerHTML=`Joined In ${data.created_at}`
+ document.getElementById('bio').innerHTML=data.bio
+   document.getElementById('following').innerHTML="folllowings : "+ data.following
+   document.getElementById('follower').innerHTML="followers : "+data.followers
+   document.getElementById('imgUrl').src=data.avatar_url
+   document.getElementById('card').style.display='block'
+})
+.catch(()=>{alert("userName not found")
+})
+}
+}
