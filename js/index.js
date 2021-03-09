13f82c55c982ddd9b8caa85df4c06ef0c7bd1eda
daniya-pro <syedadaniyahaider@gmail.com>
2021-03-09 20:52:10 +0500

    work

--- js/index.js
+++ js/index.js
@@ -14,16 +14,22 @@ acc.style.display='none'
 if(data.message===undefined){
     var datestr=new Date(data.created_at).toDateString()
 var timestr=new Date().toLocaleTimeString([],{hour12: true,hour:"2-digit",minute:'2-digit'})
-     document.getElementById('YourUserName').innerHTML=data.login
-     document.getElementById('joinedAt').innerHTML=`Joined In ${datestr} At ${timestr}`
- document.getElementById('bio').innerHTML=data.bio
-   document.getElementById('following').innerHTML="folllowings : "+ data.following
-   document.getElementById('follower').innerHTML="followers : "+data.followers
+     document.getElementById('YourUserName').innerText=data.login
+     document.getElementById('joinedAt').innerText=`Joined In ${datestr} At ${timestr}`
+ document.getElementById('bio').innerText=data.bio
+   document.getElementById('following').innerText="folllowings : "+ data.following
+   document.getElementById('follower').innerText="followers : "+data.followers
    document.getElementById('imgUrl').src=data.avatar_url
-   document.getElementById('card').style.display='block'}else{
+   document.getElementById('card').style.display='block'
+   var durl =document.getElementById("html-url")
+  
+   console.log(data.html_url,data)
+ durl.href=data.html_url
+   durl.innerText=`check ${data.login}'s URL`
+}else{
 document.getElementById('card').style.display='none'
   acc.style.display='block'
-acc.innerHTML=document.getElementById('userName').value+"'s account does not exist please try again later with a new name"
+acc.innerText=document.getElementById('userName').value+"'s account does not exist please try again later with a new name"
    }
     loader.style.display='none'
 
